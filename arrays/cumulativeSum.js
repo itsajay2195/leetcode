43e786e2f1a171770c95c3736d63a3948b87d5e1
itsajay2195 <ajaykumar.95@yahoo.com>
2022-03-23 20:42:02 +0530

    completion of positive and normal cumulative sum

--- arrays/cumulativeSum.js
+++ arrays/cumulativeSum.js
@@ -1,11 +1,12 @@
 function CumulativeSum(arr){
     let result = [arr[0]]
     for (let i =1; i< arr.length ; i++){
-        result .push(result[i-1]+arr[i])
+        let val = result[i-1]+arr[i] 
+       result.push(val)
     }   
-    console.log(result)
+    console.log(result.filter(number => number>0))
     return result
 }
 
 
-CumulativeSum([1,1,1,1,1])
+CumulativeSum([1, -1, -1, -1, 1])
